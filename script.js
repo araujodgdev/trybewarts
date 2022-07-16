189382145fb50a121b87093e3e92c49a994f4a8d
Douglas Araújo <goudxs@gmail.com>
2022-07-16 20:56:14 -0300

    Corrige requisitos do lint

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-const buttonEntrar = document.getElementById("button-entrar");
+const buttonEntrar = document.getElementById('button-entrar');
 
-buttonEntrar.addEventListener("click", (event) => {
-  const inputEmail = document.getElementById("input-Email").value;
-  const inputSenha = document.getElementById("input-senha").value;
-  const testeEmail = inputEmail === "tryber@teste.com";
-  const testeSenha = inputSenha === "123456";
+buttonEntrar.addEventListener('click', (event) => {
+  const inputEmail = document.getElementById('input-Email').value;
+  const inputSenha = document.getElementById('input-senha').value;
+  const testeEmail = inputEmail === 'tryber@teste.com';
+  const testeSenha = inputSenha === '123456';
   if (testeEmail && testeSenha) {
-    alert("Olá, Tryber!");
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
   event.preventDefault();
 });
 // segue abaixo função para o requisito 18, botão submit
-const submitBtn = document.getElementById("submit-btn");
-const agree = document.getElementById("agreement");
-submitBtn.toggleAttribute("disabled");
+const submitBtn = document.getElementById('submit-btn');
+const agree = document.getElementById('agreement');
+submitBtn.toggleAttribute('disabled');
 
 function enableSubmit() {
-  submitBtn.toggleAttribute("disabled");
+  submitBtn.toggleAttribute('disabled');
 }
 
-agree.addEventListener("change", enableSubmit);
-
+agree.addEventListener('change', enableSubmit);
 
 // segue abaixo a implementação do contador de caracteres do text area - Requisito 20;
-const textArea = document.getElementById("textarea");
-let charCounter = document.getElementById("counter");
+const textArea = document.getElementById('textarea');
+const charCounter = document.getElementById('counter');
 let charLeft = 500;
 charCounter.innerHTML = `Caracteres restantes: ${charLeft}`;
 
-textArea.addEventListener("input", () => {
-  let textoAtual = textArea.value.length;
+textArea.addEventListener('input', () => {
+  const textoAtual = textArea.value.length;
   charLeft = 500 - textoAtual;
   charCounter.innerHTML = `Caracteres restantes: ${charLeft}`;
 });
