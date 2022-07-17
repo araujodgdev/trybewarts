21c969876288885bb9070092b1c68a069a53fb45
Douglas Araújo <goudxs@gmail.com>
2022-07-17 13:36:41 -0300

    Adiciona o requisito 21

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const buttonEntrar = document.getElementById('button-entrar');
+const evalForm = document.getElementById('evaluation-form');
+const formData = document.getElementById('form-data');
+const materias = document.getElementsByName('conteudo');
+let conteudosMarcados = '';
 
 buttonEntrar.addEventListener('click', (event) => {
   const inputEmail = document.getElementById('input-Email').value;
@@ -33,4 +37,34 @@ textArea.addEventListener('input', () => {
   const textoAtual = textArea.value.length;
   charLeft = 500 - textoAtual;
   charCounter.innerHTML = `Caracteres restantes: ${charLeft}`;
+});
+
+//
+const nome = document.getElementById('data-name');
+const email = document.getElementById('data-email');
+const house = document.getElementById('data-house');
+const family = document.getElementById('data-family');
+const subjects = document.getElementById('data-content');
+const evaluation = document.getElementById('data-eval');
+const observacoes = document.getElementById('data-obs');
+
+submitBtn.addEventListener('click', (event) => {
+  const primeiroNome = document.getElementById('input-name');
+  const segundoNome = document.getElementById('input-lastname');
+  for (let index = 0; index < materias.length; index += 1) {
+    if (materias[index].checked) {
+      conteudosMarcados += `${materias[index].value}, `;
+    }
+  }
+  evalForm.style.display = 'none';
+  formData.style.display = 'block';
+  nome.innerHTML = `Nome: ${primeiroNome.value} ${segundoNome.value}`;
+  email.innerHTML = `Email: ${document.getElementById('input-email').value}`;
+  house.innerHTML = `Casa: ${document.getElementById('house').value}`;
+  family.innerHTML = `Família: ${evalForm.family.value}`;
+  subjects.innerHTML = `Matérias: ${conteudosMarcados}`;
+  evaluation.innerHTML = `Avaliação: ${evalForm.rate.value}`;
+  observacoes.innerHTML = `Observações: ${document.getElementById('textarea').value}`;
+
+  event.preventDefault();
 });
